27ba9febaaf6520a1dc6d756934edd93f097400c
Mornieur <mariafernanda-coro@hotmail.com>
2023-08-19 17:51:49 -0300

    refactor: add prettier format

--- src/components/SideBarItem/index.tsx
+++ src/components/SideBarItem/index.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import * as C from "./styles";
 import { ReactComponent as ProfileIcon } from "../../svgs/profile.svg";
@@ -21,19 +22,38 @@ export const SideBarItem = ({
 }: Props) => {
   return (
     <C.Container>
-      <Link to={path}>
+      <Link data-testid="side-bar-item" to={path}>
         <C.Info>
           <C.Title>{title}</C.Title>
           <C.Description>{description}</C.Description>
         </C.Info>
         <C.IconArea active={active}>
           {icon === "profile" && (
-            <ProfileIcon fill="white" width={24} height={24} />
+            <ProfileIcon
+              fill="white"
+              width={24}
+              height={24}
+              data-testid="profile-icon"
+            />
           )}
-          {icon === "book" && <BookIcon fill="white" width={24} height={24} />}
-          {icon === "mail" && <MailIcon fill="white" width={24} height={24} />}
+          {icon === "book" && (
+            <BookIcon
+              fill="white"
+              width={24}
+              height={24}
+              data-testid="book-icon"
+            />
+          )}
+          {icon === "mail" && (
+            <MailIcon
+              fill="white"
+              width={24}
+              height={24}
+              data-testid="mail-icon"
+            />
+          )}
         </C.IconArea>
-        <C.Point active={active}></C.Point>
+        <C.Point data-testid="active-point" active={active} />
       </Link>
     </C.Container>
   );
